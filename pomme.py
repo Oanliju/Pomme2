6435d010b7594ef07bd1babb573c660d9316be38
Oanliju <48629684+Oanliju@users.noreply.github.com>
2019-05-22 21:24:26 +0400

    Update pomme.py

--- pomme.py
+++ pomme.py
@@ -6,12 +6,12 @@ import random
 bot = commands.Bot(commands.when_mentioned_or('='))
 bot.remove_command(name="help")
 
-
+)))
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name="V1.4 | =help"))
 
-    
+    )))
 @bot.command(pass_context=True)
 async def help(ctx):
     await ctx.message.channel.purge(limit=1)
